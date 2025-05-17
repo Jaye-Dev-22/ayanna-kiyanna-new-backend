6ac3632b2dffefc53f5817f9fc2d0260a4f7728d
Kavindu Jayasinghe <87432677+Kavindu-J22@users.noreply.github.com>
2025-05-17 05:31:18 +0530

    update

--- server.js
+++ server.js
@@ -33,7 +33,16 @@ const corsOptions = {
 };
 
 // Apply CORS middleware first
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 // Handle preflight requests
 app.options('*', cors(corsOptions));
